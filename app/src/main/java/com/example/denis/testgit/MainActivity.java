46d4cfb325a808be723477cc3df6becded1df9d8
denis_chenesseau <denis.chenesseau@gmail.com>
2016-04-21 23:30:43 +0200

    Petits Toasts de Suivis sur onStart et on Resume

--- app/src/main/java/com/example/denis/testgit/MainActivity.java
+++ app/src/main/java/com/example/denis/testgit/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toast.makeText(MainActivity.this, "onCreate dude", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Toast.makeText(MainActivity.this, "onResume dude", Toast.LENGTH_SHORT).show();
     }
 
     public void sendMessage(View view){
         Intent intent=new Intent(this, DisplayMessageActivity.class);
         EditText editText=(EditText) findViewById(R.id.edit_message);
-        String message=editText.getText().toString();
+        String message= null;
+        if (editText != null) {
+            message = editText.getText().toString();
+        }
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivity(intent);
     }
